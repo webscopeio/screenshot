83a7c6673c97a17a1dba89c60a5950f965a0a124
Çağatay Karahan <kodbilenadam@gmail.com>
2023-05-17 13:57:54 +0300

    🎨 style(Button.tsx): re-order imports and add ghost variant This commit reorders the imports to follow the convention of having external libraries first, then internal modules. It also adds a new ghost variant to the buttonVariants constant.

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@utils/cn";
+import { VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-100/25 focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-slate-900",
@@ -10,6 +10,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-slate-100 text-slate-900 hover:bg-slate-200/90",
         secondary: "bg-slate-800 text-slate-100 hover:bg-slate-700/90",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
       },
       size: {
         default: "h-10 py-2 px-4",
