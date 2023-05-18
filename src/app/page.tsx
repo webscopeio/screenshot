452d850ac482f5f8dbd4c41f76ac1e95872eb9cf
Hector Sosa <hello@hectorsosa.me>
2023-05-18 11:20:03 +0200

    Settings and Preview Pane update (#49)
    * Settings and Preview Pane update
    
    * Updating borders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,40 +31,44 @@ export default function Home() {
   return (
     <LoadProvider>
       <ToastProvider>
-        <section className="grid grid-cols-[1fr_auto] w-full max-w-[90rem] place-items-end border-8 rounded-md border-slate-900/50">
-          <div className="grid w-full place-items-center h-full bg-[#020617] bg-[length:15px_15px] [background-image:radial-gradient(#64748b_0.75px,_transparent_0)] p-12">
-            <div
-              ref={clipboardRef}
-              style={{
-                padding: `${settings.padding}%`,
-              }}
-              className={`${cn(
-                "max-w-6xl max-h-[648px] grid place-items-center",
-                settings.padding === 0 && "[&>img]:rounded-none",
-                settings.aspectRatio,
-                settings.aspectRatio === "aspect-video" && "w-full",
-                !settings.isDark ? background.light : background.dark
-              )}`}
-            >
-              <ClipboardImage
-                insetColor={settings.insetColor}
-                insetPadding={settings.insetPadding}
+        <section className="flex items-center w-screen gap-4 h-screen p-4">
+          <div className="border-8 w-full h-fit rounded-md border-slate-900/50">
+            <div className="grid w-full place-items-center bg-[#020617] bg-[length:15px_15px] [background-image:radial-gradient(#64748b_0.75px,_transparent_0)] p-12">
+              <div
+                ref={clipboardRef}
+                style={{
+                  padding: `${settings.padding}%`,
+                }}
+                className={`${cn(
+                  "max-w-6xl max-h-[648px] grid place-items-center",
+                  settings.padding === 0 && "[&>img]:rounded-none",
+                  settings.aspectRatio,
+                  settings.aspectRatio === "aspect-video" && "w-full",
+                  !settings.isDark ? background.light : background.dark
+                )}`}
+              >
+                <ClipboardImage
+                  insetColor={settings.insetColor}
+                  insetPadding={settings.insetPadding}
+                  setInsetColor={setInsetColor}
+                  setInsetPadding={setInsetPadding}
+                  setIsDark={setIsDark}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="border-8 h-full min-w-[320px] rounded-md border-slate-900/50">
+            <div className="h-full flex flex-col justify-between bg-slate-900 text-slate-100 p-5">
+              <Settings
+                settings={settings}
+                setAspectRatio={setAspectRatio}
+                setPadding={setPadding}
                 setInsetColor={setInsetColor}
                 setInsetPadding={setInsetPadding}
                 setIsDark={setIsDark}
               />
+              <ActionPanel clipboardRef={clipboardRef} />
             </div>
-          </div>
-          <div className="gap-4 w-fit h-full flex flex-col justify-between p-6 min-w-[300px] bg-slate-900 text-slate-100 relative">
-            <Settings
-              settings={settings}
-              setAspectRatio={setAspectRatio}
-              setPadding={setPadding}
-              setInsetColor={setInsetColor}
-              setInsetPadding={setInsetPadding}
-              setIsDark={setIsDark}
-            />
-            <ActionPanel clipboardRef={clipboardRef} />
           </div>
         </section>
       </ToastProvider>
