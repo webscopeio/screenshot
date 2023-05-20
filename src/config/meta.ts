88484695f309ba4433ecb7151fc4d0f89369a4a8
Hector Sosa <hello@hectorsosa.me>
2023-05-20 14:08:10 +0200

    Adding branding

--- src/config/meta.ts
+++ src/config/meta.ts
@@ -4,6 +4,16 @@ export const baseMetadata: Metadata = {
   metadataBase: new URL("https://screenshot-ekqt.vercel.app/"),
   title: "Screenshot",
   description: "Build engaging screenshots faster",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      },
+    ],
+    apple: "/favicon.svg",
+    shortcut: "/favicon.svg",
+  },
   openGraph: {
     title: "Screenshot",
     description: "Build engaging screenshots faster",
