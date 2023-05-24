8990f80ea0a37b86a39aa14a73425394f036301d
Hector Sosa <hello@hectorsosa.me>
2023-05-24 22:15:14 +0200

    Adding Redirect for GitHub

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/iamhectorsosa/screenshot",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
