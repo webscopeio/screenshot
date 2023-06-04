c88461ddbc127e301a60f93109ac7770ddc33406
Hector Sosa <hello@hectorsosa.me>
2023-06-04 12:18:58 +0200

    Framing Logo (#78)

--- src/components/providers/LoadProvider.tsx
+++ src/components/providers/LoadProvider.tsx
@@ -16,7 +16,7 @@ export const LoadProvider = ({ children }: { children: React.ReactNode }) => {
 
   if (os === "undetermined" || !isReady) {
     return (
-      <div className="rounded-md bg-slate-900/90 px-4 md:px-24 md:py-12 lg:px-32 lg:py-16">
+      <div className="rounded-md bg-slate-900/90 px-4 ring-8 ring-slate-600/50 md:px-24 md:py-12 lg:px-32 lg:py-16">
         <header className="flex flex-col items-center gap-6 px-3 py-6 text-slate-200 md:px-12 md:py-9">
           <Logo />
           <Loader />
@@ -27,9 +27,11 @@ export const LoadProvider = ({ children }: { children: React.ReactNode }) => {
 
   if (os === "android" || os === "ios") {
     return (
-      <div className="rounded-md bg-slate-900/90 px-4 md:px-24 md:py-12 lg:px-32 lg:py-16">
-        <header className="flex flex-col items-center gap-6 px-3 py-6 text-slate-200 md:px-12 md:py-9">
-          <Logo />
+      <div className="rounded-md bg-slate-900/90 px-4 ring-8 ring-slate-600/50 md:px-24 md:py-12 lg:px-32 lg:py-16">
+        <header className="flex flex-col items-center gap-3 px-3 py-6 text-slate-200 md:px-12 md:py-9">
+          <div className="px-4 py-2">
+            <Logo />
+          </div>
           <p className="text-center text-lg font-semibold leading-tight text-slate-200">
             Screenshot requires a bigger screen and doesn&apos;t support
             mobile view for now.
