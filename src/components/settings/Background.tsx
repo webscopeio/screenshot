72de0ca4392f60e328b9283d1660f17e090396f2
Hector Sosa <hello@hectorsosa.me>
2023-06-16 10:12:50 +0200

    Adding backgrounds (#81)

--- src/components/settings/Background.tsx
+++ src/components/settings/Background.tsx
@@ -6,6 +6,31 @@ import { Checkbox } from "@components/ui/Checkbox";
 
 const backgroundColors = [
   {
+    name: "Violet Mesh",
+    className:
+      "bg-[hsla(289,74%,35%,1)] [background-image:radial-gradient(at_91%_72%,hsla(262,97%,60%,1)_0px,_transparent_50%),radial-gradient(at_83%_5%,hsla(255,85%,77%,1)_0px,_transparent_50%),radial-gradient(at_90%_37%,hsla(256,98%,74%,1)_0px,_transparent_50%),radial-gradient(at_59%_90%,hsla(264,94%,66%,1)_0px,_transparent_50%),radial-gradient(at_10%_40%,hsla(247,92%,70%,1)_0px,_transparent_50%),radial-gradient(at_25%_62%,hsla(319,96%,65%,1)_0px,_transparent_50%)]",
+  },
+  {
+    name: "Ocean Mesh",
+    className:
+      "bg-[hsla(203,100%,68%,1)] [background-image:radial-gradient(at_65%_26%,hsla(192,100%,57%,1)_0px,_transparent_50%),radial-gradient(at_86%_44%,hsla(59,65%,60%,1)_0px,_transparent_50%),radial-gradient(at_47%_52%,hsla(269,60%,70%,1)_0px,_transparent_50%),radial-gradient(at_86%_88%,hsla(190,86%,62%,1)_0px,_transparent_50%),radial-gradient(at_83%_1%,hsla(223,93%,70%,1)_0px,_transparent_50%),radial-gradient(at_46%_6%,hsla(236,66%,62%,1)_0px,_transparent_50%)]",
+  },
+  {
+    name: "Candy Mesh",
+    className:
+      "bg-[hsla(0,100%,50%,1)] [background-image:radial-gradient(at_40%_20%,hsla(28,100%,74%,1)_0px,_transparent_50%),radial-gradient(at_80%_0%,hsla(189,100%,56%,1)_0px,_transparent_50%),radial-gradient(at_0%_50%,hsla(355,100%,93%,1)_0px,_transparent_50%),radial-gradient(at_80%_50%,hsla(340,100%,76%,1)_0px,_transparent_50%),radial-gradient(at_0%_100%,hsla(22,100%,77%,1)_0px,_transparent_50%),radial-gradient(at_80%_100%,hsla(242,100%,70%,1)_0px,_transparent_50%),radial-gradient(at_0%_0%,hsla(343,100%,76%,1)_0px,_transparent_50%)]",
+  },
+  {
+    name: "Blue Mesh",
+    className:
+      "bg-[#a399ff] [background-image:radial-gradient(at_12%_8%,_hsla(270,85%,60%,1)_0px,_transparent_50%),radial-gradient(at_22%_39%,hsla(211,62%,64%,1)_0px,_transparent_50%),radial-gradient(at_77%_64%,hsla(151,60%,79%,1)_0px,_transparent_50%),radial-gradient(at_87%_0%,hsla(231,70%,72%,1)_0px,_transparent_50%),radial-gradient(at_51%_54%,hsla(327,96%,79%,1)_0px,_transparent_50%),radial-gradient(at_48%_32%,hsla(358,83%,72%,1)_0px,_transparent_50%),radial-gradient(at_78%_8%,hsla(173,75%,75%,1)_0px,_transparent_50%)]",
+  },
+  {
+    name: "Cotton Mesh",
+    className:
+      "bg-[hsla(0,73%,68%,1)] [background-image:radial-gradient(at_9%_4%,hsla(192,94%,55%,1)_0px,_transparent_50%),radial-gradient(at_25%_61%,hsla(300,61%,55%,1)_0px,_transparent_50%),radial-gradient(at_98%_23%,hsla(232,94%,55%,1)_0px,_transparent_50%),radial-gradient(at_26%_7%,hsla(286,64%,55%,1)_0px,_transparent_50%),radial-gradient(at_27%_46%,hsla(246,86%,55%,1)_0px,_transparent_50%),radial-gradient(at_73%_36%,hsla(290,67%,55%,1)_0px,_transparent_50%),radial-gradient(at_27%_96%,hsla(302,76%,55%,1)_0px,_transparent_50%)]",
+  },
+  {
     name: "Indigo Pink",
     className:
       "bg-gradient-to-br from-indigo-700 from-10% via-purple-600 via-30% to-pink-600 to-90% saturate-[115%]",
@@ -24,21 +49,6 @@ const backgroundColors = [
     name: "Summer Breeze",
     className:
       "bg-gradient-to-br from-yellow-400 from-0% to-pink-500 to-100% saturate-[115%]",
-  },
-  {
-    name: "Carribean Sea",
-    className:
-      "bg-gradient-to-br from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% saturate-[115%]",
-  },
-  {
-    name: "Rose Gold",
-    className:
-      "bg-gradient-to-tl from-rose-900 from-0% via-pink-500 via-50% to-rose-700 to-100% saturate-[115%]",
-  },
-  {
-    name: "Intense Blues",
-    className:
-      "bg-gradient-to-br from-[#6e01e9] from-0% via-[#3358ff] via-45% to-[#022D9B] to-100% saturate-[115%]",
   },
 ];
 
