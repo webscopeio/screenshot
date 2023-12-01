1d86419d602efd8678fbb557bf6b9eedf70cb073
iamhectorsosa <hello@hectorsosa.me>
2023-12-01 10:46:37 +0100

    Turbo fixed params

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -2,27 +2,17 @@
 
 import * as React from "react";
 import { SearchParamsProvider } from "@search-params/react";
+import { useSearchParams, useRouter } from "next/navigation";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => {
-  // TODO: Utilize URL Search Params
-  // https://github.com/vercel/next.js/issues/43691
-  const [query, setQuery] = React.useState<string>("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   return (
-    <SearchParamsProvider
-      query={query}
-      router={{
-        push: (href) => {
-          setQuery(href);
-        },
-        replace: (href) => {
-          setQuery(href);
-        },
-      }}
-    >
+    <SearchParamsProvider query={searchParams} router={router}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </SearchParamsProvider>
   );
